59bd8f9f1538ce22bdb405498c42174a7f249d10
b0ned1ger <30976199+b0ned1ger@users.noreply.github.com>
2022-04-04 14:59:54 +0300

    boilmod: module argument example
    To print stack dump:
    insmode boilmod stack=1

--- kernel-module/hello/hello.c
+++ kernel-module/hello/hello.c
@@ -3,16 +3,37 @@
 #include "func.h"
 
 MODULE_LICENSE("abc");
+
+int stack = 0;
+static int test_arguments(void);
+static void test_hello_dump_stack(void);
+
+module_param(stack, int, S_IRUGO);
+
 static int test_hello_init(void)
 {
+    test_arguments();
     printk(KERN_INFO"%s: In init\n", __func__);
     func();
     return 0;
 }
 
+static int test_arguments(void)
+{
+	if (stack)
+		test_hello_dump_stack();
+	return 0;
+}
+
 static void test_hello_exit(void)
 {
     printk(KERN_INFO"%s: In exit\n", __func__);
+}
+
+static void test_hello_dump_stack(void)
+{
+    printk(KERN_INFO"%s: dumping stack! \n", __func__);
+    dump_stack();
 }
 
 module_init(test_hello_init);
